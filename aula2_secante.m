2c801222735e8be88ea721e324c67aaaa9316426
Rui Guilherme Monteiro <61871927+rushmetra@users.noreply.github.com>
2022-02-23 19:33:17 +0000

    secante calculada

--- aula2_secante.m
+++ aula2_secante.m
@@ -1,5 +1,14 @@
+format long
 fplot(@volume,[0,4]); % plot(@function,intervalo no eixo dos xx
 grid;
+
+fx1=volume(0.25);
+fx2=volume(0.5);
+x1 = 0.25;
+x2 = 0.5;
+x3 = x2-((x2-x1).*fx2)./(fx2-fx1);
+
+
 function f = volume(x)
 f = ((pi.*x.^2.*(3-x))./3)-0.5;
 end
