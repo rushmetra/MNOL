91f87b4e92082008f63b1b740a27e2c43c863847
Rui Guilherme Monteiro <61871927+rushmetra@users.noreply.github.com>
2022-02-23 19:48:32 +0000

    Add files via upload

--- aula2_secante.m
+++ aula2_secante.m
@@ -2,6 +2,7 @@ format long
 fplot(@volume,[0,4]); % plot(@function,intervalo no eixo dos xx
 grid;
 
+% ex 12 a)
 fx1 = volume(0.25);
 fx2 = volume(0.5);
 fx3 = volume(0.4186);
@@ -16,6 +17,26 @@ cp2 = (x4-x3)./x4;
 %3 iter
 x5 = x4-((x4-x3).*fx4)./(fx4-fx3);
 fx5 = volume(0.4311)
+% ex b)
+% 
+% fx1 = volume(2.5);
+% fx2 = volume(3);
+% fx3 = volume(0.4618);
+% fx4 = volume(0.4663);
+% x1 = 0.25;
+% x2 = 0.5;
+% x3 = x2-((x2-x1).*fx2)./(fx2-fx1);
+% 
+% %2 iter
+% x4 = x3-((x3-x2).*fx3)./(fx3-fx2);
+% cp2 = (x4-x3)./x4
+% %3 iter
+% x5 = x4-((x4-x3).*fx4)./(fx4-fx3);
+% fx5 = volume(0.4311);
+
+
+
+
 function f = volume(x)
 f = ((pi.*x.^2.*(3-x))./3)-0.5;
 end
